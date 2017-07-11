f91be08c58a889417b1d2e55f4de65a0c4b53bc6
Nikolay Mitikov <nmi@sitecore.net>
2017-07-11 13:57:15 +0800

    Use code from Platform code-base

--- src/Sitecore.Support.90940/ItemResolver.cs
+++ src/Sitecore.Support.90940/ItemResolver.cs
@@ -1,169 +1,283 @@
-﻿namespace Sitecore.Support.Pipelines.HttpRequest
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="090 - ItemResolver.cs" company="Sitecore">
+//   Copyright (c) Sitecore. All rights reserved.
+// </copyright>
+// <summary>
+//   Resolves the current item from the query string path.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sitecore.Support.Pipelines.HttpRequest
 {
-    using Sitecore;
-    using Sitecore.Data.ItemResolvers;
-    using Sitecore.Data.Items;
-    using Sitecore.Data.Managers;
-    using Sitecore.Diagnostics;
-    using Sitecore.Globalization;
-    using Sitecore.IO;
-    using Sitecore.Pipelines.HttpRequest;
-    using Sitecore.SecurityModel;
-    using Sitecore.Sites;
-    using System;
-    using System.Linq;
+  using System.Linq;
+  using Sitecore.Configuration;
+  using Sitecore.Data.ItemResolvers;
+  using Sitecore.Data.Items;
+  using Sitecore.Data.Managers;
+  using Sitecore.Diagnostics;
+  using Sitecore.Globalization;
+  using Sitecore.IO;
+  using Sitecore.Pipelines.HttpRequest;
+  using Sitecore.SecurityModel;
+  using Sitecore.Sites;
 
-    public class ItemResolver : HttpRequestProcessor
+  /// <summary>
+  /// Resolves the current item from the query string path.
+  /// </summary>
+  public class ItemResolver : HttpRequestProcessor
+  {
+    /// <summary>
+    /// Item path resolver.
+    /// </summary>
+    private ItemPathResolver pathResolver;
+
+    /// <summary>
+    /// Gets or sets item path resolver.
+    /// </summary>
+    /// <value>Item path resolver.</value>
+    [NotNull]
+    protected ItemPathResolver PathResolver
     {
-        private ItemPathResolver pathResolver;
-
-        public override void Process(HttpRequestArgs args)
+      get
+      {
+        if (this.pathResolver == null)
         {
-            Assert.ArgumentNotNull(args, "args");
-            if (((Context.Item == null) && (Context.Database != null)) && (args.Url.ItemPath.Length != 0))
-            {
-                Profiler.StartOperation("Resolve current item.");
-                string path = MainUtil.DecodeName(args.Url.ItemPath);
-                Item item = args.GetItem(path);
-                if (item == null)
-                {
-                    path = args.Url.ItemPath;
-                    item = args.GetItem(path);
-                }
-                SiteContext site = Context.Site;
-                if (!args.LocalPath.Equals("/", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    if (item == null)
-                    {
-                        path = args.LocalPath;
-                        item = args.GetItem(path);
-                    }
-                    if (item == null)
-                    {
-                        path = MainUtil.DecodeName(args.LocalPath);
-                        item = args.GetItem(path);
-                    }
-                    string str2 = (site != null) ? site.RootPath : string.Empty;
-                    if (item == null)
-                    {
-                        path = FileUtil.MakePath(str2, args.LocalPath, '/');
-                        item = args.GetItem(path);
-                    }
-                    if (item == null)
-                    {
-                        path = MainUtil.DecodeName(FileUtil.MakePath(str2, args.LocalPath, '/'));
-                        item = args.GetItem(path);
-                    }
-                    if (item == null)
-                    {
-                        string name = (site != null) ? site.StartItem : string.Empty;
-                        string[] strArray = new string[] { str2, MainUtil.DecodeName(str2) };
-                        string[] startItemArray = new string[] { name, MainUtil.DecodeName(name) };
-                        string[] localPathArray = new string[] { args.LocalPath, MainUtil.DecodeName(args.LocalPath) };
-                        foreach (string str4 in (from s1 in strArray
-                                                 from s2 in startItemArray
-                                                 from s3 in localPathArray
-                                                 select FileUtil.MakePath(FileUtil.MakePath(s1, s2, '/'), s3, '/')).Distinct<string>())
-                        {
-                            item = args.GetItem(str4);
-                            if (item != null)
-                            {
-                                break;
-                            }
-                        }
-                    }
-                    if ((item == null) || item.Name.Equals("*"))
-                    {
-                        item = this.ResolveUsingDisplayName(args);
-                    }
-                }
-                if (((item == null) && args.UseSiteStartPath) && (site != null))
-                {
-                    item = args.GetItem(site.StartPath);
-                }
-                if (item != null)
-                {
-                    Tracer.Info("Current item is \"" + path + "\".");
-                }
-                Context.Item = item;
-                Profiler.EndOperation();
-            }
+          ItemPathResolver resolver = new ContentItemPathResolver();
+          this.pathResolver = (Settings.ItemResolving.FindBestMatch & MixedItemNameResolvingMode.Enabled) == MixedItemNameResolvingMode.Enabled ? new MixedItemNameResolver(resolver) : resolver;
         }
 
-        private Item ResolveFullPath(HttpRequestArgs args)
-        {
-            return (this.ResolvePath(args.Url.ItemPath) ?? this.ResolvePath(args.LocalPath));
-        }
+        return this.pathResolver;
+      }
 
-        private Item ResolveLocalPath(HttpRequestArgs args)
-        {
-            SiteContext site = Context.Site;
-            if (site == null)
-            {
-                return null;
-            }
-            Item root = ItemManager.GetItem(site.RootPath, Language.Current, Sitecore.Data.Version.Latest, Context.Database, SecurityCheck.Disable);
-            if (root == null)
-            {
-                return null;
-            }
-            string localPath = args.LocalPath;
-            return this.PathResolver.ResolveItem(localPath, root);
-        }
+      set
+      {
+        Debug.ArgumentNotNull(value, "value");
 
-        private Item ResolvePath(string path)
-        {
-            if (string.IsNullOrEmpty(path) || (path[0] != '/'))
-            {
-                return null;
-            }
-            int index = path.IndexOf('/', 1);
-            if (index < 0)
-            {
-                return null;
-            }
-            Item root = ItemManager.GetItem(path.Substring(0, index), Language.Current, Sitecore.Data.Version.Latest, Context.Database, SecurityCheck.Disable);
-            if (root == null)
-            {
-                return null;
-            }
-            string str = path.Substring(index);
-            return this.PathResolver.ResolveItem(str, root);
-        }
-
-        private Item ResolveUsingDisplayName(HttpRequestArgs args)
-        {
-            Item item;
-            Assert.ArgumentNotNull(args, "args");
-            using (new SecurityDisabler())
-            {
-                item = this.ResolveLocalPath(args);
-                if (item == null)
-                {
-                    item = this.ResolveFullPath(args);
-                }
-                if (item == null)
-                {
-                    return null;
-                }
-            }
-            return args.ApplySecurity(item);
-        }
-
-        protected ItemPathResolver PathResolver
-        {
-            get
-            {
-                if (this.pathResolver == null)
-                {
-                    this.pathResolver = new ContentItemPathResolver();
-                }
-                return this.pathResolver;
-            }
-            set
-            {
-                this.pathResolver = value;
-            }
-        }
+        this.pathResolver = value;
+      }
     }
+
+    #region Public methods
+
+    /// <summary>
+    /// Runs the processor.
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    public override void Process([NotNull] HttpRequestArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+
+      if (Context.Item != null || Context.Database == null || args.Url.ItemPath.Length == 0)
+      {
+        return;
+      }
+
+      Profiler.StartOperation("Resolve current item.");
+
+      // decoded path
+      string path = MainUtil.DecodeName(args.Url.ItemPath);
+      Item item = args.GetItem(path);
+
+      //Check if permission is denied for requested item then show Permission Denied page  and skip checking
+
+
+      // absolute path without decode
+      if (item == null && !args.PermissionDenied)
+      {
+        path = args.Url.ItemPath;
+        item = args.GetItem(path);
+      }
+      // rooted local path
+      SiteContext site = Context.Site;
+
+      string rootPath = site != null ? site.RootPath : string.Empty;
+
+
+      // local path
+      if (item == null && !args.PermissionDenied)
+      {
+        path = args.LocalPath;
+        item = args.GetItem(path);
+      }
+
+      // encoded local path
+      if (item == null && !args.PermissionDenied)
+      {
+        path = MainUtil.DecodeName(args.LocalPath);
+        item = args.GetItem(path);
+      }
+
+
+
+      if (item == null && !args.PermissionDenied)
+      {
+        path = FileUtil.MakePath(rootPath, args.LocalPath, '/');
+        item = args.GetItem(path);
+      }
+
+      // encoded rooted local path
+      if (item == null && !args.PermissionDenied)
+      {
+        path = MainUtil.DecodeName(FileUtil.MakePath(rootPath, args.LocalPath, '/'));
+        item = args.GetItem(path);
+      }
+
+      if (item == null && !args.PermissionDenied)
+      {
+        string startItem = site != null ? site.StartItem : string.Empty;
+
+        // combinations of decoded/encoded rootPath/startItem/localPath
+        var rootPathArray = new[] { rootPath, MainUtil.DecodeName(rootPath) };
+        var startItemArray = new[] { startItem, MainUtil.DecodeName(startItem) };
+        var localPathArray = new[] { args.LocalPath, MainUtil.DecodeName(args.LocalPath) };
+
+        var pathList = from s1 in rootPathArray
+                       from s2 in startItemArray
+                       from s3 in localPathArray
+                       select FileUtil.MakePath(FileUtil.MakePath(s1, s2, '/'), s3, '/');
+
+        foreach (var pathElem in pathList.Distinct())
+        {
+          item = args.GetItem(pathElem);
+
+          if (item != null)
+          {
+            break;
+          }
+        }
+      }
+
+      if ((item == null || item.Name.Equals("*")) && !args.PermissionDenied)
+      {
+        var displayNameItem = ResolveUsingDisplayName(args);
+        if (displayNameItem != null)
+        {
+          item = displayNameItem;
+        }
+      }
+
+      if (item == null && args.UseSiteStartPath && site != null && !args.PermissionDenied)
+      {
+        item = args.GetItem(site.StartPath);
+      }
+
+      if (item != null)
+      {
+        Tracer.Info("Current item is \"" + path + "\".");
+      }
+
+      Context.Item = item;
+
+      Profiler.EndOperation();
+    }
+
+    #endregion
+
+    #region Private methods
+
+    /// <summary>
+    /// Resolves the full path.
+    /// </summary>
+    /// <param name="args">The args.</param>
+    /// <returns></returns>
+    [CanBeNull]
+    Item ResolveFullPath([NotNull] HttpRequestArgs args)
+    {
+      Debug.ArgumentNotNull(args, "args");
+
+      return this.ResolvePath(args.Url.ItemPath) ?? this.ResolvePath(args.LocalPath);
+    }
+
+    /// <summary>
+    /// Resolves the path based on the url eg: args.Url.ItemPath or args.LocalPath.
+    /// </summary>
+    /// <param name="path">The path.</param>
+    /// <returns></returns>
+    Item ResolvePath(string path)
+    {
+      if (string.IsNullOrEmpty(path) || path[0] != '/')
+      {
+        return null;
+      }
+
+      int index = path.IndexOf('/', 1);
+
+      if (index < 0)
+      {
+        return null;
+      }
+
+      Item root = ItemManager.GetItem(path.Substring(0, index), Language.Current, Data.Version.Latest, Context.Database, SecurityCheck.Disable);
+
+      if (root == null)
+      {
+        return null;
+      }
+
+      string subPath = path.Substring(index);
+
+      return this.PathResolver.ResolveItem(subPath, root);
+    }
+
+    /// <summary>
+    /// Resolves the local path.
+    /// </summary>
+    /// <param name="args">The args.</param>
+    /// <returns></returns>
+    [CanBeNull]
+    Item ResolveLocalPath([NotNull] HttpRequestArgs args)
+    {
+      Debug.ArgumentNotNull(args, "args");
+
+      SiteContext site = Context.Site;
+
+      if (site == null)
+      {
+        return null;
+      }
+
+      Item root = ItemManager.GetItem(site.RootPath, Language.Current, Data.Version.Latest, Context.Database, SecurityCheck.Disable);
+
+      if (root == null)
+      {
+        return null;
+      }
+
+      string path = args.LocalPath;
+
+      return this.PathResolver.ResolveItem(path, root);
+    }
+
+    /// <summary>
+    /// Resolves the display name of the using.
+    /// </summary>
+    /// <param name="args">The args.</param>
+    /// <returns></returns>
+    [CanBeNull]
+    Item ResolveUsingDisplayName([NotNull] HttpRequestArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+
+      Item result;
+
+      using (new SecurityDisabler())
+      {
+        result = ResolveLocalPath(args);
+
+        if (result == null)
+        {
+          result = ResolveFullPath(args);
+        }
+
+        if (result == null)
+        {
+          return null;
+        }
+      }
+
+      return args.ApplySecurity(result);
+    }
+
+    #endregion
+  }
 }
